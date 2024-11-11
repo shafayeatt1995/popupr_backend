164757d772faef6d129ffb032af31d30211598a1
shafayeatt1995 <shafayetalanik@gmail.com>
2024-11-11 06:26:10 +0600

    generate api updated

--- controllers/PaymentController.js
+++ controllers/PaymentController.js
@@ -1,25 +1,17 @@
-const { Paddle, Environment } = require("@paddle/paddle-node-sdk");
-const { parseError, sendError, message, isDev, paddle } = require("../utils");
+const { parseError, sendError, message } = require("../utils");
 
 const controller = {
   async generatePaymentUrl(req, res) {
     try {
-      const {
-        PADDLE_API_KEY,
-        PADDLE_PRODUCT_ID,
-        PADDLE_PRICE_APPETIZER,
-        PADDLE_PRICE_MAIN_COURSE,
-        PADDLE_DISCOUNT_APPETIZER,
-        PADDLE_DISCOUNT_MAIN_COURSE,
-      } = process.env;
+      const { productName } = req.body;
+      if (productName) {
+        const {
+          PADDLE_PRICE_APPETIZER,
+          PADDLE_PRICE_MAIN_COURSE,
+          PADDLE_DISCOUNT_APPETIZER,
+          PADDLE_DISCOUNT_MAIN_COURSE,
+        } = process.env;
 
-      const paddle = new Paddle(PADDLE_API_KEY, {
-        environment: isDev ? Environment.sandbox : Environment.production,
-      });
-
-      const products = await paddle.products.get(PADDLE_PRODUCT_ID);
-      if (products && products.status === "active") {
-        const { productName } = req.body;
         let priceID;
         let discountID;
         if (productName === "main_course") {
@@ -33,7 +25,6 @@ const controller = {
       } else {
         sendError({ toast: message });
       }
-      return res.json({ priceData: false });
     } catch (error) {
       console.error(error);
       return res.status(500).json(parseError(error));
